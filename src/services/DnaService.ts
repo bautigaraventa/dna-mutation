02b8e0ea9a8341a180ee214a91db46ec356916b9
bautigaraventa <bautistagaraventa@hotmail.com>
2020-09-22 18:00:46 -0300

    dna mutated logic finished. unit tests left

--- src/services/DnaService.ts
+++ src/services/DnaService.ts
@@ -13,7 +13,7 @@ export class DnaService {
      */
     public hasMutation = async (dna: string[]): Promise<boolean> => {
         try {
-            const hasMutation = true; // LOGIC HERE
+            const hasMutation: boolean = this.checkDnaMutation(dna, 0, 0, 0);
 
             await dnaRepository.create({ dna, hasMutation });
 
@@ -21,6 +21,73 @@ export class DnaService {
         } catch (error) {
             throw error;
         }
+    }
+
+    /**
+     * 
+     * @param dna Array of strings representing the dna to check
+     * @param rowPosition The dna row position to process
+     * @param columnPosition The dna column position to process
+     * @param mutatedSecuences Number of mutated secuences
+     */
+    private checkDnaMutation = (dna: string[], rowPosition: number, columnPosition: number, mutatedSecuences: number): boolean => {
+        const secuences: string[] = this.getSecuencesToCheck(dna, rowPosition, columnPosition)
+
+        const filteredSecuences = secuences.filter((s) => Array.from(s).every((letter) => letter === s[0]));
+
+        const newMutatedSecuences = mutatedSecuences + filteredSecuences.length;
+
+        if (newMutatedSecuences >= 2) {
+            return true;
+        }
+
+        if (dna[rowPosition][columnPosition + 1]) {
+            return this.checkDnaMutation(dna, rowPosition, columnPosition + 1, newMutatedSecuences)
+        } else if (dna[rowPosition + 1]) {
+            return this.checkDnaMutation(dna, rowPosition + 1, 0, newMutatedSecuences);
+        }
+
+        return false;
+    }
+
+    /**
+     * 
+     * @param dna Array of strings representing the dna to check
+     * @param rowPosition The dna row position to process
+     * @param columnPosition The dna column position to process
+     */
+    private getSecuencesToCheck = (dna: string[], rowPosition: number, columnPosition: number): string[] => {
+        const secuences = [];
+
+        if (dna[rowPosition]?.[columnPosition + 3]) {
+            secuences.push(dna[rowPosition].slice(columnPosition, 4));
+        }
+
+        if (dna[rowPosition + 3]?.[columnPosition + 3]) {
+            const firstPosition = dna[rowPosition][columnPosition];
+            const secondPosition = dna[rowPosition + 1][columnPosition + 1];
+            const thirdPosition = dna[rowPosition + 2][columnPosition + 2];
+            const fourthPosition = dna[rowPosition + 3][columnPosition + 3];
+            secuences.push(firstPosition.concat(secondPosition).concat(thirdPosition).concat(fourthPosition));
+        }
+
+        if (dna[rowPosition + 3]?.[columnPosition]) {
+            const firstPosition = dna[rowPosition][columnPosition];
+            const secondPosition = dna[rowPosition + 1][columnPosition];
+            const thirdPosition = dna[rowPosition + 2][columnPosition];
+            const fourthPosition = dna[rowPosition + 3][columnPosition];
+            secuences.push(firstPosition.concat(secondPosition).concat(thirdPosition).concat(fourthPosition));
+        }
+
+        if (dna[rowPosition + 3]?.[columnPosition - 3]) {
+            const firstPosition = dna[rowPosition][columnPosition];
+            const secondPosition = dna[rowPosition + 1][columnPosition - 1];
+            const thirdPosition = dna[rowPosition + 2][columnPosition - 2];
+            const fourthPosition = dna[rowPosition + 3][columnPosition - 3];
+            secuences.push(firstPosition.concat(secondPosition).concat(thirdPosition).concat(fourthPosition));
+        }
+
+        return secuences;
     }
 
     /**
@@ -36,7 +103,7 @@ export class DnaService {
 
             const count_mutations: number = dnaStats.filter((ds: any) => ds.hasMutation).length;
             const count_no_mutations: number = dnaStats.length - count_mutations;
-            
+
             const ratio: number = count_mutations ? count_no_mutations ? count_mutations / count_no_mutations : 1 : 0;
 
             return {
